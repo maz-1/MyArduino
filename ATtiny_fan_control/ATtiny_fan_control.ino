a6909ce0c4cee410b5e28da8b7c210fb17063af9
You <you@example.com>
2018-06-14 17:21:13 +0800

    init

---
+++ ATtiny_fan_control/ATtiny_fan_control.ino
@@ -0,0 +1,153 @@
+Z/*
+ *                                     ATtiny85
+ *                                  -------u-------
+ *              RST - A0 - (D 5) --| 1 PB5   VCC 8 |-- +5V
+ *                                 |               |
+ *                                 |               |
+ *      Relay     <-- A3 - (D 3) ==| 2 PB3   PB2 7 |-- (D 2) - A1  --> Voltage Divider
+ *(turn 4-wire fan on/off)         |               |  (made of 2k resistor and 5k B3470 thermistor)
+ *                                 |               |
+ *                    A2 - (D 4) ==| 3 PB4   PB1 6 |-- (D 1) - PWM --> Fan PWM
+ *                                 |               |  (connect to fan pwm pin or a logic-level mosfet)
+ *                                 |               |      
+ *                          Gnd ---| 4 GND   PB0 5 |-- (D 0) - PWM --> LED
+ *                                 -----------------
+ *                                 
+ */
+
+
+// normal delay() won't work anymore because we are changing Timer1 behavior
+// Adds delay_ms and delay_us functions
+#include <util/delay.h>
+
+// Clock at 8mHz
+//#define F_CPU 8000000  // This is used by delay.h library
+//#define PWMTOP 39
+//#define PWMBOTTOM 19
+
+// Clock at 16mHz
+#define F_CPU 16000000  // This is used by delay.h library
+#define PWMTOP 79
+ #define PWMBOTTOM 39
+
+// Temperature (vqoltage divider made of 2k resistor and 5k B3470 thermistor)
+// 35C 400 | 40C 412 | 50C 500
+
+// turn off voltage 35C
+#define VOL_OFF 400
+// turn on voltage 40C
+#define VOL_ON 412
+// max voltage 50C
+#define VOL_MAX 500
+
+// PWM signal, Only works with Pin 1(PB1)
+#define PWMPin 1
+// PB2 get analog input
+#define PotPin A1
+// PB3 Enable PWM Fan via relay
+#define EnPin 3
+// PB0 LED
+#define LEDPin 0
+
+int FanOn = 0;
+int FanMax = 0;
+int LedStat = 0;
+
+void setup()
+{
+  pinMode(PWMPin, OUTPUT);
+  pinMode(EnPin, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
+  led_off();
+}
+
+void loop()
+{
+  int in;
+  
+  in = analogRead(PotPin);
+      
+  if (in > VOL_ON || (in > VOL_OFF && FanOn))
+  {
+    if (!FanOn)
+    {
+      FanOn = 1;
+      enable_pwm();
+    }
+    if (in < VOL_MAX)
+    {
+      if (FanMax)
+      {
+        FanMax = 0;
+        enable_pwm();
+      }
+      OCR0B = map(in, VOL_OFF, VOL_MAX, PWMBOTTOM, PWMTOP);
+      led_on();
+    }
+    else
+    {
+      disable_pwm(1);
+      FanMax = 1;
+      led_toggle();
+    }
+  }
+  else if (FanOn)
+  {
+    FanOn = 0;
+    disable_pwm(0);
+    led_off();
+  }
+  
+  _delay_ms(200);
+}
+
+void disable_pwm(int mode)
+{
+  if (mode)
+  {
+    digitalWrite(EnPin, HIGH);
+    _delay_ms(100);
+    digitalWrite(PWMPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(PWMPin, LOW);
+    digitalWrite(EnPin, LOW);
+  }
+}
+
+void enable_pwm()
+{
+  digitalWrite(EnPin, HIGH);
+  disable_pwm(1);
+  _delay_ms(1000);
+  // Fast PWM Mode, Prescaler = /8
+  // PWM on Pin 1(PB1), Pin 0(PB0) disabled
+  // 8Mhz / 8 / (39 + 1) = 25Khz
+  TCCR0A = _BV(COM0B1) | _BV(WGM01) | _BV(WGM00);
+  TCCR0B = _BV(WGM02) | _BV(CS01);
+  //TCCR0B = _BV(WGM02) | _BV(CS02);
+  // Set TOP and initialize duty cycle to zero(0)
+  OCR0A = PWMTOP;    // TOP - DO NOT CHANGE, SETS PWM PULSE RATE
+  OCR0B = PWMBOTTOM;     // duty cycle for Pin 1(PB1) - generates 1 500nS pulse even when 0
+}
+
+void led_on()
+{
+  LedStat = 1;
+  digitalWrite(LEDPin, HIGH);
+}
+
+void led_off()
+{
+  LedStat = 0;
+  digitalWrite(LEDPin, LOW);
+}
+
+void led_toggle()
+{
+  if (LedStat)
+    led_off();
+  else
+    led_on();
+}
---
+++ ESP8266_AutoConnect/ESP8266_AutoConnect.ino
@@ -0,0 +1,125 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+void tick()
+{
+  //toggle state
+  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
+  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  //set led pin as output
+  pinMode(BUILTIN_LED, OUTPUT);
+  // start ticker with 0.5 because we start in AP mode and try to connect
+  //ticker.attach(0.6, tick);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  wifiManager.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect()) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+  ticker.detach();
+  //keep LED on
+  digitalWrite(BUILTIN_LED, LOW);
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  int val;
+  if (req.indexOf("/gpio/0") != -1)
+    val = LOW;
+  else if (req.indexOf("/gpio/1") != -1)
+    val = HIGH;
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  // Set GPIO2 according to the request
+  digitalWrite(BUILTIN_LED, val);
+  
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
+  s += (val)?"high":"low";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+
+}
+
